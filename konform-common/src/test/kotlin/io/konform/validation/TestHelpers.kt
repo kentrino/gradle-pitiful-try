d62d4a9c1ec2fcc5dbbd1894b5a1b72b8224b9ad
kentrino <kentrino@h-kento.jp>
2019-03-05 15:35:05 +0900

    remove test

--- konform-common/src/test/kotlin/io/konform/validation/TestHelpers.kt
+++
@@ -1,5 +0,0 @@
-package io.konform.validation
-
-fun <T> countFieldsWithErrors(validationResult: ValidationResult<T>) = (validationResult as Invalid).errors.size
-fun countErrors(validationResult: ValidationResult<*>, vararg properties: Any) = validationResult.get(*properties)?.size
-    ?: 0
