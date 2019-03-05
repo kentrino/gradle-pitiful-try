aa2fe3cc0e67de59c74e59c520c2508f16abbf4a
kentrino <kentrino@h-kento.jp>
2019-03-05 10:54:57 +0900

    :tada: initial commit

---
+++ api/src/Application.kt
@@ -0,0 +1,62 @@
+package jp.hkento
+
+import com.fasterxml.jackson.databind.SerializationFeature
+import io.ktor.application.Application
+import io.ktor.application.call
+import io.ktor.application.install
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.cio.CIO
+import io.ktor.features.CallLogging
+import io.ktor.features.ContentNegotiation
+import io.ktor.features.StatusPages
+import io.ktor.http.ContentType
+import io.ktor.http.HttpStatusCode
+import io.ktor.jackson.jackson
+import io.ktor.response.respond
+import io.ktor.response.respondText
+import io.ktor.routing.get
+import io.ktor.routing.routing
+import io.ktor.server.engine.commandLineEnvironment
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+import io.ktor.util.KtorExperimentalAPI
+import org.koin.ktor.ext.inject
+import org.koin.ktor.ext.installKoin
+import java.util.*
+
+
+fun main(args: Array<String>) {
+    embeddedServer(Netty, commandLineEnvironment(args)).start()
+}
+
+@KtorExperimentalAPI
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+    install(CallLogging)
+
+    install(ContentNegotiation) {
+        jackson {
+            configure(SerializationFeature.INDENT_OUTPUT, true)
+        }
+    }
+
+    install(StatusPages) {
+        exception<Throwable> { cause ->
+            call.respond(HttpStatusCode.InternalServerError)
+        }
+    }
+
+    val random by inject<Random>()
+
+
+    routing {
+        get("/") {
+            call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
+        }
+
+        get("/error") {
+            throw Exception("test")
+        }
+    }
+}
